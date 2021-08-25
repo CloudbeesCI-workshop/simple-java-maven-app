280fd9c3bceb406391fb83fbcb8ba0773eb92355
Mandeep Kaur <45703878+MandeepKaurJS@users.noreply.github.com>
2021-08-25 14:33:55 -0700

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,14 +4,16 @@ package com.mycompany.app;
  * Hello world app.
  */
 public class App {
+
   private final String message = "Hello World!";
+
   public App() {}
 
   public static void main(String[] args) {
     System.out.println(new App().getMessage());
   }
 
-  private final String getMessage() {
+  private String getMessage() {
     return message;
   }
 }
